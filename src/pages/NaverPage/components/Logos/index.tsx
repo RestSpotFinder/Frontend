dd9d040ea8bea577362447df6b1c9f7bcfeb404a
hayamaster <dnfxmfkdbzos@naver.com>
2024-03-02 16:33:04 +0900

    feat: 출발지, 도착지 커스텀 마크 적용

--- src/pages/NaverPage/components/Logos/index.tsx
+++ src/pages/NaverPage/components/Logos/index.tsx
@@ -2,7 +2,7 @@ import naver_logo from '../../../../assets/naver_logo.png'
 
 const Logos = () => {
   return (
-    <div className="bg-white h-full w-16 relative left-0 top-0 bottom-0 border ">
+    <div className="relative bottom-0 left-0 top-0 h-full w-16 border bg-white ">
       <h1 className="naverLogo">
         <img src={naver_logo} alt="naver_logo" />
       </h1>
