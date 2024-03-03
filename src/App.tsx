44129a99ba5f3a988dbd0c10fbc96def165cffc8
hayamaster <dnfxmfkdbzos@naver.com>
2024-03-04 01:41:34 +0900

    feat: react query 설치 및 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
 
+const queryClient = new QueryClient()
+
 function App() {
   return (
-    <div className="h-dvh">
-      <RouterProvider router={router} />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="h-dvh">
+        <RouterProvider router={router} />
+      </div>
+    </QueryClientProvider>
   )
 }
 
