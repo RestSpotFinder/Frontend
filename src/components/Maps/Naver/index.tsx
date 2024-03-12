9323f5507e85ce9e9ada07aa1fb151c0966c0769
hayamaster <dnfxmfkdbzos@naver.com>
2024-03-12 19:15:22 +0900

    feat: 네이버 맵에 경로에 따른 휴게소 마커 추가

--- src/components/Maps/Naver/index.tsx
+++ src/components/Maps/Naver/index.tsx
@@ -4,16 +4,16 @@ import {
   useNavermaps,
   Polyline,
 } from 'react-naver-maps'
-import { useEffect, useState, Dispatch, SetStateAction } from 'react'
-import { CustomMarker } from '@/components'
-import { Place } from '@/types'
-import { useGetRoutes } from '@/apis/hooks'
+import { useEffect, useState, Dispatch, SetStateAction, useRef } from 'react'
+import { CustomMarker, RestSpotMarker } from '@/components'
+import { Place, Route, RestSpot } from '@/types'
+import { useGetRoutes, useGetRestSpots } from '@/apis/hooks'
 
 interface NaverProps {
   start: Place | null
   goal: Place | null
   waypoints?: Place[]
-  selectedRouteOption: string
+  selectedRouteOption?: string
   setSelectedRouteOption?: Dispatch<SetStateAction<string>>
 }
 
@@ -24,11 +24,14 @@ const Naver = ({
     { lat: '37.4449168', lng: '127.1388684' },
     { lat: '37.8847972', lng: '127.7169083' },
   ],
-  selectedRouteOption = 'fast',
-  setSelectedRouteOption,
+  // selectedRouteOption = 'fast',
+  // setSelectedRouteOption,
 }: NaverProps) => {
   const navermaps = useNavermaps()
-  const [map, setMap] = useState<naver.maps.Map | null>(null)
+  const mapRef = useRef<naver.maps.Map>(null)
+  const [init, setInit] = useState<boolean>(true)
+  const [selectedRoute, setSelectedRoute] = useState<Route>()
+  const [restSpots, setRestSpots] = useState<RestSpot[]>()
   const { data: routes } = useGetRoutes({
     start: [start?.lng, start?.lat].join(','),
     goal: [goal?.lng, goal?.lat].join(','),
@@ -37,57 +40,82 @@ const Naver = ({
     ),
     page: '1',
   })
+  const { data: restSpotData } = useGetRestSpots({
+    routeId: selectedRoute?.routeId,
+  })
 
   useEffect(() => {
-    if (map && !start && !goal) {
+    setSelectedRoute(
+      routes.find(route => route.routeOption === selectedRoute?.routeOption),
+    )
+  }, [routes, setSelectedRoute, selectedRoute])
+
+  useEffect(() => {
+    if (restSpotData) {
+      setRestSpots(restSpotData)
+    }
+  }, [restSpotData, setRestSpots])
+
+  useEffect(() => {
+    if (init) {
       navigator.geolocation.getCurrentPosition(
         position => {
           const { latitude, longitude } = position.coords
           const myPosition = { lat: latitude, lng: longitude }
 
-          map.setCenter(myPosition)
+          mapRef.current?.setCenter(myPosition)
         },
         error => {
           console.error(error)
         },
       )
+      setSelectedRoute(routes[0])
+      setInit(false)
     }
-  }, [map, start, goal])
+  }, [mapRef, init, routes])
 
   useEffect(() => {
-    if (start && map) {
-      map.setCenter(
+    if (start && init) {
+      console.log('출발지 변경')
+      mapRef.current?.setCenter(
         new naver.maps.LatLng(parseFloat(start.lat), parseFloat(start.lng)),
       )
-      map.setZoom(15)
+      mapRef.current?.setZoom(15)
     }
-  }, [map, start])
+  }, [mapRef, start, init])
 
   useEffect(() => {
-    if (goal && map) {
-      map.setCenter(
+    if (goal && init) {
+      console.log('목적지 변경')
+      mapRef.current?.setCenter(
         new naver.maps.LatLng(parseFloat(goal.lat), parseFloat(goal.lng)),
       )
-      map.setZoom(15)
+      mapRef.current?.setZoom(15)
     }
-  }, [map, goal])
+  }, [mapRef, goal, init])
 
   useEffect(() => {
-    if (selectedRouteOption && map && start && goal) {
-      map.setCenter({
+    if (start && goal && init) {
+      console.log('출발지 목적지 설정')
+      mapRef.current?.setCenter({
         lat: (parseFloat(start.lat) + parseFloat(goal.lat)) / 2,
         lng: (parseFloat(start.lng) + parseFloat(goal.lng)) / 2,
       })
-      map.setZoom(10)
+      mapRef.current?.setZoom(10)
     }
-  }, [selectedRouteOption, map, start, goal])
+  }, [mapRef, start, goal, init])
+
+  const handleClickRoute = (routeOption: string) => {
+    setSelectedRoute(routes.find(route => route.routeOption === routeOption))
+    console.log(routeOption)
+  }
 
   return (
     <MapDiv style={{ width: '100%', height: '100dvh' }}>
       <NaverMap
         defaultCenter={new navermaps.LatLng(37.3595704, 127.105399)}
         defaultZoom={12}
-        ref={setMap}
+        ref={mapRef}
       >
         {start && (
           <CustomMarker
@@ -121,6 +149,18 @@ const Naver = ({
               />
             )
           })}
+        {restSpots &&
+          restSpots.map((spot, idx) => {
+            return (
+              <RestSpotMarker
+                position={{
+                  lat: spot.lat,
+                  lng: spot.lng,
+                }}
+                key={idx}
+              />
+            )
+          })}
         {start &&
           goal &&
           routes.map(path => (
@@ -134,15 +174,12 @@ const Naver = ({
               })}
               strokeLineCap="round"
               strokeLineJoin="round"
-              strokeColor={`${path.routeOption === selectedRouteOption ? '#2DB400' : '#A9A9A9'}`}
+              strokeColor={`${path.routeOption === selectedRoute?.routeOption ? '#2DB400' : '#A9A9A9'}`}
               strokeOpacity={0.8}
               strokeWeight={6}
               clickable={true}
-              onClick={() =>
-                setSelectedRouteOption &&
-                setSelectedRouteOption(path.routeOption)
-              }
-              zIndex={selectedRouteOption === path.routeOption ? 1 : 0}
+              onClick={() => handleClickRoute(path.routeOption)}
+              zIndex={selectedRoute?.routeOption === path.routeOption ? 1 : 0}
             />
           ))}
       </NaverMap>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { NaverMap } from './Maps'
 export { default as CustomMarker } from './CustomMarker'
+export { default as RestSpotMarker } from './RestSpotMarker'
