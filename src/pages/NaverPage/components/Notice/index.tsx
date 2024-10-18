8d0634f852cc7b31baf352850999843b61c068af
Gupuroom <cjsrnr1114@naver.com>
2024-10-18 09:13:43 +0900

    fix: build 에러 해결 - 사용 안하는 import 제거

--- src/pages/NaverPage/components/Notice/index.tsx
+++ src/pages/NaverPage/components/Notice/index.tsx
@@ -1,19 +1,23 @@
 import useGetAnnounces from '@/apis/hooks/useGetAnnounces'
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
-import { GrAnnounce } from 'react-icons/gr'
 import { RocketIcon } from '@radix-ui/react-icons'
 
 const Notice = () => {
   const { data: announceList } = useGetAnnounces()
-  
+
   const recentAnnounce = announceList && announceList[0]
-  if(!announceList) return <div></div>
+  if (!announceList) return <div></div>
+
   return (
     <div className="px-[2rem]">
-      <Alert className='bg-[#E5F1FE] border-none'>
-        <RocketIcon color='#0475F5' className="h-4 w-4" />
-        <AlertTitle className='text-[#0475F5] font-bold'>{recentAnnounce?.title}</AlertTitle>
-        <AlertDescription className='text-[#2b85ec]'>{recentAnnounce?.content}</AlertDescription>
+      <Alert className="border-none bg-[#E5F1FE]">
+        <RocketIcon color="#0475F5" className="h-4 w-4" />
+        <AlertTitle className="font-bold text-[#0475F5]">
+          {recentAnnounce?.title}
+        </AlertTitle>
+        <AlertDescription className="text-[#2b85ec]">
+          {recentAnnounce?.content}
+        </AlertDescription>
       </Alert>
     </div>
   )
