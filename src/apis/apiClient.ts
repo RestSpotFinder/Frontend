7453a6257cae3d4c4c77c03e3bdf88624d693568
Gupuroom <cjsrnr1114@naver.com>
2025-03-17 10:07:04 +0900

    fix: 도메인 변경 - restspotfinder.site -> restspotfinder.kr

--- src/apis/apiClient.ts
+++ src/apis/apiClient.ts
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 const apiClient = axios.create({
   baseURL: import.meta.env.DEV
-    ? 'http://61.97.187.101:8080/api'
-    : 'https://restspotfinder.site/api',
+    ? 'http://localhost:8080/api'
+    : 'https://www.restspotfinder.kr/api',
 })
 
 export default apiClient
