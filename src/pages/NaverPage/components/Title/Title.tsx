0773a9cb32f97e1422086279fa4c768c90680cc9
dongjae <cjsrnr1114@naver.com>
2025-07-09 00:15:43 +0900

    refactor: Title 컴포넌트 CSS를 Tailwind로 변경

--- src/pages/NaverPage/components/Title/Title.tsx
+++ src/pages/NaverPage/components/Title/Title.tsx
@@ -1,9 +1,9 @@
- import './Title.css' // CSS 파일을 import 합니다.
-
 const Title = () => {
   return (
-    <div className="title">
-      <span>경로상 휴게소 찾기</span>
+    <div className="block px-4 pb-2 pt-6 text-center">
+      <span className="text-2xl font-medium text-green-600/70">
+        경로상 휴게소 찾기
+      </span>
     </div>
   )
 }
